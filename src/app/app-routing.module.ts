eda80aa7b9c420c781c87c226493754fb864bfa7
bayeget <a.bayegetov@gmail.com>
2019-10-28 20:21:53 +0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Tab4Page } from './tab4/tab4.page';
+import { Tab4PageModule } from './tab4/tab4.module';
+import { Tab1Page } from './tab1/tab1.page';
+import { Tab2Page } from './tab2/tab2.page';
+import { Tab3Page } from './tab3/tab3.page';
+import { Tab1PageModule } from './tab1/tab1.module';
+import { Tab2PageModule } from './tab2/tab2.module';
+import { Tab3PageModule } from './tab3/tab3.module';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'tab1', component: Tab1Page },
+  { path: 'tab2', component: Tab2Page },
+  { path: 'tab3', component: Tab3Page },
+  { path: 'tab4', component: Tab4Page },
+
 ];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    Tab1PageModule,
+    Tab2PageModule,
+    Tab3PageModule,
+    Tab4PageModule
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
